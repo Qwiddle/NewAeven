c7911243ecbc593567f7fa56706bf443470a1e73
Andrew Rubenstein <andjruben@gmail.com>
2022-06-26 21:41:12 -0400

    Fix player creation UI swap

--- client/js/actions/player/create.mjs
+++ client/js/actions/player/create.mjs
@@ -2,7 +2,7 @@ import { ViewLoader } from "../../ui/viewLoader.mjs";
 import { global } from "../../global.mjs";
 export class PlayerCreateAction {
 	static init(account, reservation) {
-		ViewLoader.removeView("home", true, () => {
+		ViewLoader.removeView(ViewLoader.currentView, true, () => {
 			ViewLoader.loadView("charactercreation", true);
 		});
 
@@ -33,7 +33,7 @@ export class PlayerCreateAction {
 		const params = {
 			method: 'POST',
 			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({player: playerData, reservation: reservation})
+			body: JSON.stringify({ player: playerData, reservation: reservation })
 		};
 
 		const response = await fetch('/user/create', params);
@@ -45,13 +45,14 @@ export class PlayerCreateAction {
 		}
 
 		const data = await response.json();
+		console.log(data);
 
 		if(!data.output.player) {
 			//temporary alert, should be handled by ui
 			alert(`HTTP error! status: ${response.status}`);
 			return false;
 		} else {
-			return {player: data.output.player, seatReservation: reservation};
+			return { player: data.output.player, seatReservation: reservation };
 		}
 	}
 }
